8e0414d3cbe1584b0fd145065f62ebc255782763
purnimamehta <contact@purnimamehta.me>
2016-06-09 17:06:24 -0400

    initial commmit

---
+++ adventure.py
@@ -0,0 +1,189 @@
+import sys
+from game_data import Player, Location, Item
+
+class World:
+    """A class to store all map, location and item data for game world."""
+
+    def __init__(self, mapfile, locfile, itemfile):
+        """
+        (World, str filename, str filename, str filename) -> None
+        
+        Initialize a World using data from given filenames.
+        """
+        
+        self.map = []
+        self.locations = {}
+        self.items = {}
+
+        self.load_map(mapfile)
+        self.load_locations(locfile)
+        self.load_items(itemfile)
+
+    def load_map(self, filename):
+        '''
+        (World, str filename) -> None
+        Store map from filename in self.map as a nested list
+        of strings like so:
+            1 2 5
+            3 -1 4
+        becomes [['1','2','5'], ['3','-1','4']]
+        The given 'filename' is a string that gives name of text file
+        in which map data is located.
+        '''
+
+        pass
+    
+    def load_locations(self, filename):
+        '''
+        (World, str filename) -> None
+        Store all locations from filename in self.locations
+        as a dictionary where integer location numbers are the keys,
+        and Location objects are the values:
+        {integer location number: Location object}.
+        
+        The given 'filename' is a string that gives name of text file
+        in which location data is located.
+        '''
+
+        # TODO:
+        # Open and read given filename
+        # For each location, create a new Location object with data from file.
+        #   e.g. new_location = Location(location id, short desc, long desc, ...)
+        # Add this location to self.locations dict with key as integer
+        # of location id and value as this new_location object
+
+    def load_items(self, filename):
+        '''
+        (World, str filename) -> None
+        Store all items from filename in self.items
+        with item name as the key, and an Item object as the value.
+        Each item should also be added into self.locations as
+        based on the item's starting location as given in text file.
+        
+        The given 'filename' is a string that gives name of text file
+        in which item data is located.
+        '''
+
+        # TODO:
+        # Open and read given filename
+        # For each item, create a new Item object with data from file.
+        # Then, two things must be done:
+        # 1. Using item's starting location, append Item object
+        #    to that starting location's ".items" list
+        #    i.e. (add Item to self.locations[starting_location_id].items)
+        # 2. Store item in self.items dict with key as str item name
+        #    and value as Item object
+
+    def get_room(self, x, y):
+        '''
+        (World, int, int) -> Location
+        Return the Location object associated with x, y values given.
+        '''
+
+        # TODO:
+        # 1. Get location id at given x, y coordinates in self.map list
+        # 2. Return Location object associated with this location number in self.locations dictionary
+
+        pass
+
+    def get_moves(self, x, y):
+        '''
+        (World, int, int) -> lst of str
+        The given x and y represent the player's current position.
+        Using x, y coordinates, check self.map list for what directions the player can currently move in.
+        Return list of possible moves based on map data and boundaries.
+        e.g. The returned list should look something like: ["go north", "go south"]
+        '''
+
+        pass
+
+# --- END of World class --- #
+
+
+# --- Functions to handle player's actions in the game ---
+
+def view_inventory():
+    '''
+    Suggested helper function.
+    You may use or remove this as you see fit.
+    TODO: Update this docstring based on how you use this method.
+    '''
+
+    pass
+
+def use_item(current_item, current_location):
+    '''
+    Suggested helper function.
+    You may use or remove this as you see fit.
+    TODO: Update this docstring based on how you use this method.
+    '''
+
+    pass
+
+def get_actions(current_location):
+    '''
+    (Location) -> lst of strs
+    Return a list of strings where each string represents
+    a possible action the user can take from the given current_location.
+    e.g. The returned list should look something like:
+         ["go north", "go south", "take t-card"]
+    '''
+    
+    actions = []
+
+    # TODO:
+    # Use WORLD.get_moves to append all possible moves to actions.
+    # Append any other possible actions (e.g. 'take item' option for
+    # each available item from current room), etc.
+
+    return actions
+
+def do_action(choice, current_loc):
+    '''
+    (str, Location) -> None
+    This function takes str choice and Location current_loc and
+    carries out consequences of making the given choice.
+    You may update this docstring based on how you choose to code this function.
+    '''
+
+    # TODO:
+    # Carry out consequences of making the given choice
+    # e.g.
+    if choice == "go north":
+        PLAYER.move_north()
+    # ... handle any other possible choices ...
+
+
+if __name__ == "__main__":
+
+    # Variables to store world info and player info
+    WORLD = World("map.txt", "locations.txt", "items.txt")
+    PLAYER = Player(0, 0) # set starting location of player; you may change the x, y coordinates here as appropriate
+
+    menu = ["look", "inventory", "score", "quit"]
+
+    while not PLAYER.victory:
+
+        # Variable to keep track of player's current location
+        # Get current location based on player's x and y coordinates
+        current_loc = WORLD.get_room(PLAYER.x, PLAYER.y)
+
+        # TODO: ENTER CODE HERE TO PRINT LOCATION DESCRIPTION
+        # Depending on whether or not it's been visited before,
+        # print either full description (first time visit) or brief description (every subsequent visit)
+
+        print("\nWhat to do? \n")
+        available_actions = get_actions(current_loc)
+        for action in available_actions:
+            print(action)
+
+        print("===")
+        print("Menu Options: \n")
+        for option in menu:
+            print(option)
+
+        choice = input("\nChoose action: ")
+
+        # Check that choice is valid in this situation
+        # If it is, call do_action(choice, current_loc),
+        # If not, print appropriate error message
---
+++ game_data.py
@@ -0,0 +1,186 @@
+class Location:
+    '''Class to represent each location in game world.'''
+
+    def __init__(self):
+        '''Creates a new location.          
+        
+        Data that should be associated with each Location object:
+        an integer location ID,
+        a brief description,
+        a long description,
+        items that are available in the location,
+        and whether or not the location has been visited before.
+
+        You may change/add parameters and the data available for each
+        Location class as you see fit.
+        e.g. You may want to add special actions that could be available for
+             some locations.
+             
+        All locations in your game MUST be represented as an instance of this class.
+        '''
+
+        # TODO: Add new attributes for this class here to store location data.
+        # e.g. a list to store items available in location (store as Item objects)
+        self.items = []
+
+    def get_brief_description (self):
+        '''
+        (Location) -> str
+        Return str brief description of location.'''
+        
+        pass
+
+    def get_full_description (self):
+        '''
+        (Location) -> str
+        Return str long description of location.'''
+        
+        pass
+
+    # TODO: Add other 'getter' methods as above for other location data as needed
+
+class Item:
+    '''Class to represent each item in game world.'''
+
+    def __init__ (self, name, start, target, target_points):
+        '''Create item referred to by string name, with integer "start"
+        being the integer identifying the item's starting location,
+        the integer "target" being the item's target location, and
+        integer target_points being the number of points player gets
+        if item is deposited in target location.
+
+        You may change these parameters and the data available for each
+        Item class as you see fit.
+        Consider every method in this Item class as a "suggested method":
+        -- Suggested Method (You may remove/modify/rename these as you like) --
+
+        The only thing you must NOT change is the name of this class: Item.
+        All item objects in your game MUST be represented as an instance of this class.
+        '''
+
+        self.name = name
+        self.start = start
+        self.target = target
+        self.target_points = target_points
+
+    def get_starting_location (self):
+        '''
+        (Item) -> int
+        Return location (the integer id) of where item is first found.'''
+
+        pass
+
+    def get_name(self):
+        '''
+        (Item) -> str
+        Return the str name of the item.'''
+
+        pass
+
+    def get_target_location (self):
+        '''
+        (Item) -> int
+        Return item's target location (the integer id) of where it should be deposited.'''
+
+        pass
+
+    def get_target_points (self):
+        '''
+        (Item) -> int
+        Return integer points awarded for depositing the item in its target location.'''
+
+        pass
+
+class Player:
+    '''Class to represent the player in game world.'''
+
+    def __init__(self, x, y):
+        '''
+        (Player, int, int) -> None
+        Creates a new Player at given x and y coordinates.
+
+        You may add new parameters / attributes / methods to this class as you see fit.
+        '''
+        self.x = x
+        self.y = y
+        self.victory = False # set to True once player has won game
+        self.score = 0
+        self.inventory = [] # store list of item names that are in player's inventory
+
+    def move(self, dx, dy):
+        '''
+        (Player, int, int) -> None
+        Given integers dx and dy, move player to new location (self.x + dx, self.y + dy)
+        '''
+
+        pass
+
+    def move_north(self):
+        '''
+        (Player) -> None
+        Helper function to move Player one position north on map.
+        These integer directions are based on how the map must be stored
+        in our nested list self.map.
+        '''
+        self.move(0,-1)
+
+    def move_south(self):
+        '''
+        (Player) -> None
+        Helper function to move Player one position south on map.
+        These integer directions are based on how the map must be stored
+        in our nested list self.map.
+        '''
+        self.move(0,1)
+
+    def move_east(self):
+        '''
+        (Player) -> None
+        Helper function to move Player one position east on map.
+        These integer directions are based on how the map must be stored
+        in our nested list self.map.
+        '''
+
+        # TODO: use self.move to move one east
+        
+        pass
+
+    def move_west(self):
+        '''
+        (Player) -> None
+        Helper function to move Player one position west on map.
+        These integer directions are based on how the map must be stored
+        in our nested list self.map.
+        '''
+
+        # TODO: use self.move to move one west
+
+        pass
+
+    def add_item(self, item):
+        '''
+        (Player, str item name) -> None
+        Add given item name to player's inventory.
+        :param item:
+        :return:
+        '''
+
+        pass
+
+    def remove_item(self, item):
+        '''
+        (Player, str item name) -> None
+        Remove given item name from player's inventory.
+        '''
+
+        pass
+
+    def get_inventory(self):
+        '''
+        (Player) -> lst
+        Return player's inventory.
+        '''
+
+        pass
+    
+    
