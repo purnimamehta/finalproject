2f5a15c979c470f891bffab2eb3cd82e1c497705
Manjot singh Nijjar <manjotn24@gmail.com>
2016-06-19 10:41:19 -0400

    # 1. Using item's starting location, append Item object         #    to that starting location's ".items" list         #    i.e. (add Item to self.locations[starting_location_id].items)
    not done rest of items down i don’t know starting locations

--- adventure.py
+++ adventure.py
@@ -118,12 +118,29 @@ class World:
         # 2. Store item in self.items dict with key as str item name
         #    and value as Item object
 
-        file = open(filename,'r')
-        items_list=[]
+
 
         for line in file:
             line.split(" ")[0].strip("\n")
             items_list.append(line)
+            self.items[line]
+
+        file = open(filename,'r')
+        items_list=[]
+        items_dict={}
+        i=0
+
+        for line in file:
+            line.split(" ")[0].strip("\n")
+            items_list.append(line)
+            self.items[line] = self.items
+            self.items[line]=items_list[i]
+            i= i + 1
+
+
+
+
+        file.close()
 
 
 
