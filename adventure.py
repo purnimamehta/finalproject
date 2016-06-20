0b2dcf50aee9ed35174784cc327e242f09a19794
Manjot singh Nijjar <manjotn24@gmail.com>
2016-06-19 20:34:54 -0400

    points

--- adventure.py
+++ adventure.py
@@ -81,11 +81,11 @@ class World:
                 long_description = line.strip("long description:").strip("\n")
 
 
-            if "1" or "2" or "3" or "4" or "5" in line:
+            if "1" or "2" or "3" or "4" or "5" or "6" or "7" or "8" or "9" or "10" or "11" or "12" or "13" or "14" or "15" in line:
                 new_locations[key] = line
 
             if "Item" in line:
-                new_locations[key] = line    
+                new_locations[key] = line
 
 
         self.locations = new_locations
